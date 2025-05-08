e98b1da3c1e6bc24171cb0e8ec7658e1691e63e5
github-ci <you@example.com>
2025-05-08 09:37:33 +0000

    fix++

--- src/Entity/ScanLog.php
+++ src/Entity/ScanLog.php
@@ -4,7 +4,6 @@ namespace WechatOfficialAccountQrcodeBundle\Entity;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Attribute\Groups;
 use Tourze\DoctrineIndexedBundle\Attribute\IndexColumn;
 use Tourze\DoctrineTimestampBundle\Attribute\CreateTimeColumn;
 use Tourze\EasyAdmin\Attribute\Column\ExportColumn;
@@ -32,7 +31,6 @@ class ScanLog
     #[ListColumn(order: 98, sorter: true)]
     #[ExportColumn]
     #[CreateTimeColumn]
-    #[Groups(['restful_read', 'admin_curd'])]
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '创建时间'])]
     private ?\DateTimeInterface $createTime = null;
 

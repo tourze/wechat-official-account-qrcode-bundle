67ef65dd4e580612495607ebc1305ea6a3333ffd
github-ci <you@example.com>
2025-05-18 11:04:25 +0000

    refactor: 修改 ScanLog 实体以使用 UserInterface 接口

--- src/Entity/ScanLog.php
+++ src/Entity/ScanLog.php
@@ -8,7 +8,7 @@ use Tourze\DoctrineIndexedBundle\Attribute\IndexColumn;
 use Tourze\DoctrineTimestampBundle\Attribute\CreateTimeColumn;
 use Tourze\EasyAdmin\Attribute\Column\ExportColumn;
 use Tourze\EasyAdmin\Attribute\Column\ListColumn;
-use WechatOfficialAccountBundle\Entity\User;
+use Tourze\WechatOfficialAccountContracts\UserInterface;
 use WechatOfficialAccountQrcodeBundle\Repository\ScanLogRepository;
 
 #[ORM\Entity(repositoryClass: ScanLogRepository::class, readOnly: true)]
@@ -22,18 +22,6 @@ class ScanLog
     #[ORM\Column(type: Types::INTEGER, options: ['comment' => 'ID'])]
     private ?int $id = 0;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    #[IndexColumn]
-    #[ListColumn(order: 98, sorter: true)]
-    #[ExportColumn]
-    #[CreateTimeColumn]
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '创建时间'])]
-    private ?\DateTimeInterface $createTime = null;
-
     #[ORM\ManyToOne(inversedBy: 'scanLogs')]
     #[ORM\JoinColumn(nullable: false)]
     private ?QrcodeTicket $qrcode = null;
@@ -42,18 +30,18 @@ class ScanLog
     private ?string $openId = null;
 
     #[ORM\ManyToOne]
-    private ?User $user = null;
+    private ?UserInterface $user = null;
 
-    public function setCreateTime(?\DateTimeInterface $createdAt): self
+    #[IndexColumn]
+    #[ListColumn(order: 98, sorter: true)]
+    #[ExportColumn]
+    #[CreateTimeColumn]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '创建时间'])]
+    private ?\DateTimeInterface $createTime = null;
+
+    public function getId(): ?int
     {
-        $this->createTime = $createdAt;
-
-        return $this;
-    }
-
-    public function getCreateTime(): ?\DateTimeInterface
-    {
-        return $this->createTime;
+        return $this->id;
     }
 
     public function getQrcode(): ?QrcodeTicket
@@ -80,15 +68,27 @@ class ScanLog
         return $this;
     }
 
-    public function getUser(): ?User
+    public function getUser(): ?UserInterface
     {
         return $this->user;
     }
 
-    public function setUser(?User $user): static
+    public function setUser(?UserInterface $user): static
     {
         $this->user = $user;
 
         return $this;
+    }
+
+    public function setCreateTime(?\DateTimeInterface $createdAt): self
+    {
+        $this->createTime = $createdAt;
+
+        return $this;
+    }
+
+    public function getCreateTime(): ?\DateTimeInterface
+    {
+        return $this->createTime;
     }
 }
